932d3662c90ef0b5eb4a3ba850964496edbff065
klawton1 <klawton1@gmail.com>
2018-02-03 16:40:03 -0800

    moved script to app.js

--- app.js
+++ app.js
@@ -4,3 +4,35 @@ var app = new Vue({
     message: 'You should not go here!'
   }
 })
+tomtom.key("b3ZvXNJXHCtWtPogGdS2eBkWMsXhRG6T");
+var map;
+var startPos;
+var geoSuccess = function(position) {
+  map = tomtom.map("map");
+  startPos = position;
+  map.setView([startPos.coords.latitude, startPos.coords.longitude], 14);
+
+  var marker = tomtom.L.marker([startPos.coords.latitude, startPos.coords.longitude], {
+    draggable: true
+  }).bindPopup('Current Location').addTo(map);
+
+  marker.on('dragend', function(e) {
+    tomtom.reverseGeocode({position: e.target.getLatLng()})
+    .go(function(response) {
+      if (response && response.address && response.address.freeformAddress) {
+        marker.setPopupContent(response.address.freeformAddress);
+      } else {
+        marker.setPopupContent('No results found');
+      }
+      marker.openPopup();
+    });
+  });
+};
+results = new Vue({
+  el: "#results",
+  data: {
+    items: ['hello']
+  }
+})
+navigator.geolocation.getCurrentPosition(geoSuccess);
+    
