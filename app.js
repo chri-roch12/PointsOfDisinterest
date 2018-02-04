8e7d8c06e2efe40ac466a2080182391dbea21fd0
Jesse Schettler <jesse@Jesses-MacBook-Pro.local>
2018-02-03 18:08:01 -0800

    Added ability to select start and end points for trip

--- app.js
+++ app.js
@@ -4,35 +4,67 @@ var app = new Vue({
     message: 'You should not go here!'
   }
 })
-tomtom.key("b3ZvXNJXHCtWtPogGdS2eBkWMsXhRG6T");
-var map;
-var startPos;
-var geoSuccess = function(position) {
-  map = tomtom.map("map");
-  startPos = position;
-  map.setView([startPos.coords.latitude, startPos.coords.longitude], 14);
 
-  var marker = tomtom.L.marker([startPos.coords.latitude, startPos.coords.longitude], {
-    draggable: true
-  }).bindPopup('Current Location').addTo(map);
-
-  marker.on('dragend', function(e) {
-    tomtom.reverseGeocode({position: e.target.getLatLng()})
-    .go(function(response) {
-      if (response && response.address && response.address.freeformAddress) {
-        marker.setPopupContent(response.address.freeformAddress);
-      } else {
-        marker.setPopupContent('No results found');
-      }
-      marker.openPopup();
-    });
-  });
-};
 results = new Vue({
   el: "#results",
   data: {
     items: ['hello']
   }
 })
-navigator.geolocation.getCurrentPosition(geoSuccess);
-    
+
+tomtom.key('xmAGh1TQiNYjuwZsDOJL4SpOjhVdWns7');
+var map = tomtom.map("map");
+map.zoomControl.setPosition('topright');
+var unitSelector = tomtom.unitSelector.getHtmlElement(tomtom.globalUnitService);
+var languageSelector = tomtom.languageSelector.getHtmlElement(tomtom.globalLocaleService, 'search');
+var unitRow = document.createElement('div');
+var unitLabel = document.createElement('label');
+unitLabel.innerHTML = 'Unit of measurement';
+unitLabel.appendChild(unitSelector);
+unitRow.appendChild(unitLabel);
+unitRow.className = 'input-container';
+var langRow = document.createElement('div');
+var langLabel = document.createElement('label');
+langLabel.innerHTML = 'Search language';
+langLabel.appendChild(languageSelector);
+langRow.appendChild(langLabel);
+langRow.className = 'input-container';
+tomtom.controlPanel({
+    position: 'bottomright',
+    title: 'Settings',
+    collapsed: true
+})
+    .addTo(map)
+    .addContent(unitRow)
+    .addContent(langRow);
+// Relocating zoom buttons
+map.zoomControl.setPosition('bottomleft');
+// Adding the route widget
+var routeOnMapView = tomtom.routeOnMap({
+    // Options for the route start marker
+    startMarker: {
+        icon: tomtom.L.icon({
+            iconUrl: 'jssdk-4/images/start.png',
+            iconSize: [30, 30],
+            iconAnchor: [15, 15]
+        })
+    },
+    // Options for the route end marker
+    endMarker: {
+        icon: tomtom.L.icon({
+            iconUrl: 'jssdk-4/images/end.png',
+            iconSize: [30, 30],
+            iconAnchor: [15, 15]
+        })
+    }
+}).addTo(map);
+// Creating route inputs widget
+var routeInputsInstance = tomtom.routeInputs({location: false})
+    .addTo(map);
+// Connecting the route inputs widget with the route widget
+routeInputsInstance.on(routeInputsInstance.Events.LocationsFound, function(eventObject) {
+    routeOnMapView.draw(eventObject.points);
+});
+routeInputsInstance.on(routeInputsInstance.Events.LocationsCleared, function(eventObject) {
+    routeOnMapView.draw(eventObject.points);
+});
